fc90d44d4cddb21ed666bf764b2d7bf484143619
Szymon Konieczny <szykon@gmail.com>
2018-05-08 16:42:36 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import './styles/styles.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
