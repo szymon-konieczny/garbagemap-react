775ac637db776933f1eab6cc82c2dc3590e47f22
Szymon Konieczny <szykon@gmail.com>
2018-05-14 03:14:10 +0200

    build update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
 import './styles/styles.scss';
 
-ReactDOM.render(<Router basename={process.env.PUBLIC_URL}><App /></Router>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
