e3e28da91aaf3ff7d1d653b0e0a2117ad2093a39
Szymon Konieczny <szykon@gmail.com>
2018-05-28 13:05:51 +0200

    menu fix

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import LogIn from './LogIn';
 import LogOut from './LogOut';
+import '../styles/components/Auth.scss';
 
 const Auth = (props) => (
-  <React.Fragment>
+  <React.Fragment className="auth-wrapper">
     { props.user ?
       <LogOut 
         logout={ props.logout } 
